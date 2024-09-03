ff2514d8ab55de53e1e45804a71393bfcf2903fa
Edward crowe <96554603+0asaca0rum0@users.noreply.github.com>
2024-09-03 17:22:51 +0100

    v1.3

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -106,9 +106,9 @@ export default function Projects() {
     return (
         <Tabs value={activeTab} onChange={(value) => setActiveTab(value)} >
             <TabsHeader
-                className="h-full w-full   rounded-2xl p-2 m-4 bg-gray-600 bg-opacity-15   backdrop-blur-lg backdrop-filter max-w-full min-w-full mx-auto"
+                className="h-full w-full   rounded-2xl px-5 py-2 md:p-2 m-4 bg-gray-600 bg-opacity-15   backdrop-blur-lg backdrop-filter max-w-full min-w-full mx-auto"
                 indicatorProps={{
-                    className: "bg-white px-2 bg-opacity-10 shadow-[0px_4px_12px_rgba(0,0,0,0.1),_0px_8px_32px_rgba(0,0,0,0.08),_inset_0px_1px_1px_rgba(255,255,255,0.2)]  py-3 mx-2  rounded-xl",
+                    className: "bg-white  bg-opacity-10 shadow-[0px_4px_12px_rgba(0,0,0,0.1),_0px_8px_32px_rgba(0,0,0,0.08),_inset_0px_1px_1px_rgba(255,255,255,0.2)]  py-3 -mx-1 md:mx-4  rounded-xl",
                 }}
                 id="project"
             >
